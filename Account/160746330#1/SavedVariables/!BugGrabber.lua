975fdad6504db6ed0b387547ac8ac127db42e043
zhangtailun <zhang280907842@163.com>
2019-03-14 22:04:56 +0800

    配置更新

--- Account/160746330#1/SavedVariables/!BugGrabber.lua
+++ Account/160746330#1/SavedVariables/!BugGrabber.lua
@@ -1,13 +1,7 @@
 
 BugGrabberDB = {
+	["session"] = 4966,
 	["lastSanitation"] = 3,
-	["session"] = 4965,
 	["errors"] = {
-		{
-			["message"] = "Frame MYSLOT_ReportFrame_EditBox: Unknown script element OnClick",
-			["session"] = 4965,
-			["time"] = "2019/03/14 16:44:55",
-			["counter"] = 16,
-		}, -- [1]
 	},
 }
